8252f2b1b35a4cc22c5a60f85440e2e9f4258133
Puneet_bhatnagar <91620854+puneetbhatnagarr@users.noreply.github.com>
2022-01-22 22:36:42 +0530

    Update settings.py

--- projectosp/settings.py
+++ projectosp/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/photos')
 STATIC_URL = '/static/'
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
-
+STATICFILES_DIRS=['static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
