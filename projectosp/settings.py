2520211f55156d2ad3dbf0c722f8f2f28cd2dc80
Puneet_bhatnagar <91620854+puneetbhatnagarr@users.noreply.github.com>
2022-01-23 11:57:34 +0530

    Update settings.py

--- projectosp/settings.py
+++ projectosp/settings.py
@@ -127,7 +127,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,  'templates'),
+    # Add to this list all the locations containing your static files 
+)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
