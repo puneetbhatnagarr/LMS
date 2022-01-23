84550e9691ffa561738c57a3bc45b8983ba0ecb2
Puneet_bhatnagar <91620854+puneetbhatnagarr@users.noreply.github.com>
2022-01-23 15:49:52 +0530

    Update settings.py

--- projectosp/settings.py
+++ projectosp/settings.py
@@ -134,9 +134,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
-STATICFILES_DIRS=(
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS=['static']
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
