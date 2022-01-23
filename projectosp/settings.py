665212b153a5269df08e610a8cadd836e764e72f
Puneet_bhatnagar <91620854+puneetbhatnagarr@users.noreply.github.com>
2022-01-23 11:01:37 +0530

    Update settings.py

--- projectosp/settings.py
+++ projectosp/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE_CLASSES = (
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    ...
+    ...)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 heroku config:set DISABLE_COLLECTSTATIC=1
 
